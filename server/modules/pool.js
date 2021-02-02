6747048cbd6ebcf9f202f36029bc5a3e6656144b
NateMacLaurin <brow1982@umn.edu>
2021-02-02 15:09:22 -0600

    setup .env

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -28,8 +28,8 @@ if (process.env.DATABASE_URL) {
   };
 } else {
   config = {
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
+    host: process.env.PG_USER || 'localhost', // Server hosting the postgres database
+    port: process.env.PG_SECRET || 5432, // env var: PGPORT
     database: 'secure_submarine', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
